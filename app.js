1ffe5e8932f0c674cd451e0f5dee2270b36cc052
Jake Chavez <jake94chavez@gmail.com>
2017-12-15 12:26:15 -0600

    Cleaned up Javascript and added descriptive comments.

--- app.js
+++ app.js
@@ -1,35 +1,40 @@
 const allPosts = [];
 
+// Runs post functionality if button is clicked
 $( '#button' ).click(function() {
-	// alert('nice click!');
-	let $toPost = $('#submission').val();
-	if ($toPost == '') {
-		alert('There is nothing to add to the list!')
-	} else {
-	allPosts.push($toPost);
-	$('#submission').val(null);
-	makeAPost();
-	}
+	fieldValue();
 });
 
+// Runs post functionality if enter is pressed
 $('#submission').keyup(event => {
 	if (event.which == 13) {
-	// alert('nice click!');
-	let $toPost = $('#submission').val();
-	if ($toPost == '') {
-		alert('There is nothing to add to the list!')
-	} else {
-	allPosts.push($toPost);
-	$('#submission').val(null);
-	makeAPost();
+		fieldValue();
 	}
-}
 });
 
+
+// Puts the new list item on the post 
 const makeAPost = () => {
+	// Adds the newest value from an array that stores all posts into a list element
 	let $history = $('#feed');
 	let $listItem = $("<li>");
 	let $postText = allPosts[allPosts.length-1];
+	// Adds the new list element to the HTML
 	$listItem.append($postText);
 	$history.append($listItem);	
+}
+
+// Sets the input to be added to the list and alerts the user if nothing is in the input field
+const fieldValue = () => {
+	// Stores the input value
+	let $toPost = $('#submission').val();
+	// Stops the function and alerts the user if there is nothing in the field
+	if ($toPost == '') {
+		alert('There is nothing to add to the list!')
+	} else {
+	// If the field has a value the value is added to an array and then reset to null
+	allPosts.push($toPost);
+	$('#submission').val(null);
+	makeAPost();
+	}
 }
